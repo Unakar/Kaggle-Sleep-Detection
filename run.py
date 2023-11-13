c3c3b1477dabfae15201035da9f1482c8e2ee3f8
ybbwcwaps <gjr13754822885@mail.ustc.edu.cn>
2023-11-13 21:28:43 +0800

    改为run.py来配环境

---
+++ run.py
@@ -0,0 +1,9 @@
+torch-2.1.0+cu118-cp310-cp310-linux_x86_64.whl 
+    torchaudio-2.1.0+cu118-cp310-cp310-linux_x86_64.whl 
+    torchvision-0.16.0+cu118-cp310-cp310-linux_x86_64.whl
+    segmentation-models-pytorch 
+    pytorch_lightning 
+    hydra-core --upgrade
+    polars
+    transformers
+    -U 'wandb>=0.12.10'
