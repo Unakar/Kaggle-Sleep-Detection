25cafd6a4be7910ecb9854c04f2c9fcd6f031fe0
ybbwcwaps <gjr13754822885@mail.ustc.edu.cn>
2023-11-13 21:02:28 +0800

    run.sh 百度环境配置

---
+++ run.sh
@@ -0,0 +1,13 @@
+pip install torch-2.1.0+cu118-cp310-cp310-linux_x86_64.whl torchaudio-2.1.0+cu118-cp310-cp310-linux_x86_64.whl torchvision-0.16.0+cu118-cp310-cp310-linux_x86_64.whl
+
+pip install segmentation-models-pytorch 
+
+pip install pytorch_lightning 
+
+pip install hydra-core --upgrade
+
+pip install polars
+
+pip install transformers
+
+pip install -U 'wandb>=0.12.10'
