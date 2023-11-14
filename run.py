30c406e580011b772ce9e0d2d5ebe787e0a674aa
sigma aster <60509017+sigma-aster@users.noreply.github.com>
2023-11-14 21:43:08 +0800

    初始化时运行安装需要的库

--- run.py
+++ run.py
@@ -1,19 +1,27 @@
 import subprocess
+import zipfile
 
-# 定义要安装的库列表
+zip_path ='/home/aistudio/data/data247831/torch.zip'
+save_path = '/home/aistudio/data/data247831/'
+
+file=zipfile.ZipFile(zip_path)
+file.extractall(save_path)
+
 libraries = [
-    'torch-2.1.0+cu118-cp310-cp310-linux_x86_64.whl',
-    'torchaudio-2.1.0+cu118-cp310-cp310-linux_x86_64.whl',
-    'torchvision-0.16.0+cu118-cp310-cp310-linux_x86_64.whl',
-    'segmentation-models-pytorch' ,
+    '{}torch-2.1.0+cu118-cp310-cp310-linux_x86_64.whl'.format(save_path),
+    '{}torchaudio-2.1.0+cu118-cp310-cp310-linux_x86_64.whl'.format(save_path),
+    '{}torchvision-0.16.0+cu118-cp310-cp310-linux_x86_64.whl'.format(save_path),
+    'segmentation-models-pytorch',
     'pytorch_lightning' ,
-    'hydra-core --upgrade',
     'polars',
     'transformers',
-    "-U 'w'andb>=0.12.10"
+    "-U 'wandb>=0.12.10'"
 ]
 
 # 循环安装库
 for library in libraries:
     print(f"安装 {library}...")
     subprocess.call(['pip', 'install', library])
+
+print(f"升级 {'hydra-core'}...")
+subprocess.call(['pip', 'install', 'hydra-core', '--upgrade'])
